71dc2fa92d3d567368e3435ab1ea1d0655c41f1e
Shrey <shreynavadia@Shreys-Laptop.attlocal.net>
2024-06-16 12:20:28 -0500

    Upload, Rename, Delete Working

--- file_client.py
+++ file_client.py
@@ -3,23 +3,51 @@ import time
 import grpc
 import file_service_pb2
 import file_service_pb2_grpc
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler, FileModifiedEvent, FileDeletedEvent, FileCreatedEvent, FileMovedEvent
 
 SYNC_FOLDER = "sync_folder/"
 SERVER_ADDRESS = 'localhost:50051'
 
-def upload_file(stub, file_name):
-    with open(os.path.join(SYNC_FOLDER, file_name), 'rb') as f:
+class FileEventHandler(FileSystemEventHandler):
+    def __init__(self, stub):
+        self.stub = stub
+
+    def on_modified(self, event):
+        if isinstance(event, FileModifiedEvent) and not event.is_directory:
+            if os.path.exists(event.src_path):
+                upload_file(self.stub, event.src_path)
+
+    def on_created(self, event):
+        if isinstance(event, FileCreatedEvent) and not event.is_directory:
+            if os.path.exists(event.src_path):
+                upload_file(self.stub, event.src_path)
+
+    def on_deleted(self, event):
+        if isinstance(event, FileDeletedEvent) and not event.is_directory:
+            delete_file(self.stub, event.src_path)
+
+    def on_moved(self, event):
+        if isinstance(event, FileMovedEvent) and not event.is_directory:
+            rename_file(self.stub, event.src_path, event.dest_path)
+
+def upload_file(stub, file_path):
+    file_name = os.path.basename(file_path)
+    with open(file_path, 'rb') as f:
         content = f.read()
     file = file_service_pb2.File(name=file_name, content=content)
     response = stub.UploadFile(file)
     print(response.message)
 
-def delete_file(stub, file_name):
+def delete_file(stub, file_path):
+    file_name = os.path.basename(file_path)
     file = file_service_pb2.FileName(name=file_name)
     response = stub.DeleteFile(file)
     print(response.message)
 
-def rename_file(stub, old_name, new_name):
+def rename_file(stub, old_path, new_path):
+    old_name = os.path.basename(old_path)
+    new_name = os.path.basename(new_path)
     request = file_service_pb2.RenameRequest(old_name=old_name, new_name=new_name)
     response = stub.RenameFile(request)
     print(response.message)
@@ -27,18 +55,18 @@ def rename_file(stub, old_name, new_name):
 def monitor_folder():
     channel = grpc.insecure_channel(SERVER_ADDRESS)
     stub = file_service_pb2_grpc.FileServiceStub(channel)
-
-    last_checked = time.time()
-
-    while True:
-        time.sleep(5)
-        current_files = os.listdir(SYNC_FOLDER)
-        for file_name in current_files:
-            file_path = os.path.join(SYNC_FOLDER, file_name)
-            if os.path.getmtime(file_path) > last_checked:
-                upload_file(stub, file_name)
-
-        last_checked = time.time()
+    
+    event_handler = FileEventHandler(stub)
+    observer = Observer()
+    observer.schedule(event_handler, SYNC_FOLDER, recursive=True)
+    observer.start()
+    
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
 
 if __name__ == '__main__':
     if not os.path.exists(SYNC_FOLDER):
